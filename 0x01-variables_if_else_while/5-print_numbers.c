fa399f0580761bab3d70e80c2508a23ddec4905a
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-08 13:06:59 -0700

    hello

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	int i;
 
-int main() {
-    int i;
-    
-    for (i = 0; i < 10; i++) {
-        printf("%d\n", i);
-    }
-    
-    return 0;
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
 }
