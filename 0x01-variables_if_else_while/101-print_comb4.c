6fcc229785beebc9b9c17700349068c73b683f4f
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-08 14:34:46 -0700

    hello

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,6 @@
  * Return: 0
 */
 int main(void)
-
 {
 int i, j, k;
 for (i = 0; i < 8; i++)
@@ -17,7 +16,7 @@ for (k = j + 1; k < 10; k++)
 putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
-if (i != 7  j != 8  k != 9)
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
