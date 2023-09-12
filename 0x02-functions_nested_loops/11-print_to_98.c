c96ee70d6a195f0590d4606c838ddf055c7bd4f6
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-12 15:52:11 -0700

    hello

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,41 +7,38 @@
  * @num: The number to start printing from
  * Return: Always 0.
  */
-
 void print_to_98(int num)
-
 {
-
-	if (num <= 98)
-	{
-	for (; num <= 98; num++)
-	{
-	if (num  == 98)
-	{
-	printf("%d", num);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d, ", num);
-	}
-	}
-	}
-	else
-	{
-	for (; num >= 98; num--)
-	{
-	if (num == 98)
-	{
-	printf("%d", num);
-        printf("\n");
-        break;
-        }
-        else
-        {       
-        printf("%d, ", num);
-	}
-	}
-	}
+if (num <= 98)
+{
+for (; num <= 98; num++)
+{
+if (num  == 98)
+{
+printf("%d", num);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", num);
+}
+}
+}
+else
+{
+for (; num >= 98; num--)
+{
+if (num == 98)
+{
+printf("%d", num);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", num);
+}
+}
+}
 }
