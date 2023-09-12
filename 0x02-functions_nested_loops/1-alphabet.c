44770d1a95f3d6c08b54701040efeed7d0d9f250
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-12 07:30:50 -0700

    hello

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ * print_alphabet - prints a to z
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+
+/**
+ * main - main function
+ * Return: 0
+*/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
