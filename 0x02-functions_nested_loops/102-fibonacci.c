c52eda08487d0f20a37383f6d401d35cdc3d917a
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-12 17:57:59 -0700

    Answer.

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main  Prints the first 52 fibonacci numbers
+ * main - Prints the first 52 fibonacci numbers
  * Return: Noting!
  */
 
@@ -9,23 +9,24 @@ int main(void)
 
 {
 
-	int i = 0;
-	long j = 1, k = 2;
+int i = 0;
+long j = 1, k = 2;
 
-	while (i < 50)
-	{
-	if (i == 0)
-	printf("%ld", j);
-	else if (i == 1)
-	printf(", %ld", k);
-	else
-	{
-	k += j;
-	j = k - j;
-	printf(", %ld", k);
-	}
-	++i;
-	}
-	printf("\n");
-	return (0);
+while (i < 50)
+{
+if (i == 0)
+printf("%ld", j);
+else if (i == 1)
+printf(", %ld", k);
+else
+{
+k += j;
+j = k - j;
+printf(", %ld", k);
 }
+++i;
+}
+printf("\n");
+return (0);
+}
+
