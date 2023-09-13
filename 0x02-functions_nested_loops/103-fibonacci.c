4dfba23e3e34a78b5a47faec216ebc2d5badf6de
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-12 17:33:50 -0700

    Answe for 15.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the sum of even Fibonacci numbers
+ *  Return: 0 in case of success, -1 in case of error
+*/
+int main(void)
+{
+int i = 0;
+long j = 1, k = 2, sum = k;
+while (k + j <= 4000000)
+{
+k += j;
+if (k % 2 == 0)
+sum += k;
+j = k - j;
+++i;
+
+}
+printf("%d\n", sum);
+return (0);
+}
+
