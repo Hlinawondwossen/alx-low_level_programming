64ae62c56f5d748994225c887da63b848475873e
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-13 14:46:49 -0700

    hello

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests  function that prints if integer is positive or  negative 
+ * main - to test the iny is positive or negative  
  * Return: 0
  */
 int main(void)
 
 {
-	int i;
+	int x;
 
-	i = 0;
-	positive_or_negative(i);
+	x = 0;
+	positive_or_negative(x);
 	return (0);
 }
 
