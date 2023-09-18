29c40a6611c3604a8113b98dd6d54d277c0199e0
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-18 14:09:17 -0700

    hello

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,16 @@
  */
 
 void print_array(int *a, int n)
+{
+	int j;
 
-{
-int j;
-
-for (j = 0; j < n; j++)
-{
-printf("%d", a[j]);
-if (j != (n - 1))
-{
-printf(" ,");
+	for (j = 0; j < n; j++)
+	{
+	printf("%d", a[j]);
+	if (j != (n - 1))
+	{
+	printf(" ,");
+	}
+	}
+	printf("\n");
 }
-}
-printf("\n");
-i}
