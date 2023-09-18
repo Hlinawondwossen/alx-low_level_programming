2b4ce37e42bb921162b94dc8855269f715a63292
HlinaWondwossen <hlinawondwossen2022@gmail.com>
2023-09-18 13:40:02 -0700

    hello

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy -Copy a string 
+ * _strcpy -Copy a string
  * @dest: Destination value
  * @src: Source value
  * Return: the pointer to dest
@@ -9,11 +9,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
-	for (i = 0; src[i] != '\0'; i++)
-	{
-	dest[i] = src[i];
-	}
-	dest[i++] = '\0';
-	return (dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
