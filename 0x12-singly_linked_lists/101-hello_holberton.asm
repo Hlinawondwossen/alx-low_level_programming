e7777b38201758bb407d562d5e1bf77bb2bef213
hlinawondwossen <hlinawondwossen2022@gmail.com>
2023-10-19 19:05:36 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,17 @@
- global    main
-          extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 10, 0
 
+section .text
+    extern printf
+
+global main
+main:
+    sub rsp, 8                         ; Align stack
+    mov rdi, format                    ; Format string
+    mov rsi, hello                     ; String to print
+    xor eax, eax                       ; Clear EAX register
+    call printf                        ; Call printf function
+    add rsp, 8                         ; Restore stack
+    xor eax, eax                       ; Return 0
+    ret
