84c9a1080d823e08a1df48db5575f2c986c4481e
hlinawondwossen <hlinawondwossen2022@gmail.com>
2023-10-25 13:37:12 -0700

    Answer question 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+size_t node_count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		/* Updating pointer 'h' to point to next node */
-		h = h->next;
-		node_count++;
-	}
+while (h != NULL)
+{
+printf("%d\n", h->n);
+/* Updating pointer 'h' to point to next node */
+h = h->next;
+node_count++;
+}
 
-	return (node_count);
+return (node_count);
 }
