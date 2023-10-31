1d90b8a9caf905a2a7e209f74f917875d0bb1333
hlinawondwossen <hlinawondwossen2022@gmail.com>
2023-10-30 19:23:56 -0700

    hello

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,45 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content written in the file.
+ * create_file - Creates a file with specific permissions and writes content.
+ * @filename: The Name of file to create.
+ * @text_content: Text string to write to the file.
  *
- * Return: 1 if it succeeds, -1 if it fails.
+ * Return: 1 on success, -1 on failure.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-        int fd;
-        int nletters;
-        int rwr;
 
-        if (!filename)
-                return (-1);
+	FILE *fptr;
+	size_t txtlen = 0;
+	size_t bytes_written;
 
-        // Open the file with the appropriate flags and permissions
-        fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (filename == NULL)
+		return (-1);
 
-        if (fd == -1)
-                return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
-        if (!text_content)
-                text_content = "";
+	
+	while (text_content[txtlen] != '\0')
+		txtlen++;
 
-        // Count the number of characters in the text_content
-        for (nletters = 0; text_content[nletters]; nletters++)
-                ;
+	
+	fptr = fopen(filename, "w");
+	if (fptr == NULL)
+		return (-1); 
 
-        // Write the content to the file
-        rwr = write(fd, text_content, nletters);
+	
+	bytes_written = fwrite(text_content, 1, txtlen, fptr);
+	fclose(fptr);
 
-        if (rwr == -1)
-                return (-1);
+	if (bytes_written != txtlen)
+		return (-1); 
 
-        // Close the file descriptor
-        close(fd);
+	
+	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
+		return (-1); 
 
-        return (1);
-}}
+	return (1);
+}
